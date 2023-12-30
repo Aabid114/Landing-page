19e9fa836b8577f16e278bd9af4123adda3b0924
Aabid114 <155263428+Aabid114@users.noreply.github.com>
2023-12-31 03:27:21 +0530

    Add files via upload
    To do list

---
+++ todolist.java
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Your Apple Supplying Company<<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Your Apple Supplying Company</title>
+    <!-- Add your CSS links for styling here -->
+</head>
+
+<body>
+    <header>
+        <h1>Welcome to Your Apple Supplying Company</h1>
+        <!-- Add navigation links here -->
+    </header>
+
+    <section>
+        <h2>Featured Products</h2>
+        <!-- Add your product display here -->
+    </section>
+
+    <section>
+        <h2>About Us</h2>
+        <!-- Add information about your company here -->
+    </section>
+
+    <section>
+        <h2>Contact Us</h2>
+        <!-- Add a contact form and details here -->
+    </section>
+
+    <footer>
+        <p>&copy; 2023 Your Apple Supplying Company</p>
+    </footer>
+
+    <!-- Add your JavaScript links for functionality here -->
+</body>
+
+</html>
+/title>
+    <!-- Add your CSS links for styling here -->
+</head>
+
+<body>
+    <header>
+        <h1>Welcome to Your Apple Supplying Company</h1>
+        <!-- Add navigation links here -->
+    </header>
+
+    <section>
+        <h2>Featured Products</h2>
+        <!-- Add your product display here -->
+    </section>
+
+    <section>
+        <h2>About Us</h2>
+        <!-- Add information about your company here -->
+    </section>
+
+    <section>
+        <h2>Contact Us</h2>
+        <!-- Add a contact form and details here -->
+    </section>
+
+    <footer>
+        <p>&copy; 2023 Your Apple Supplying Company</p>
+    </footer>
+
+    <!-- Add your JavaScript links for functionality here -->
+</body>
+
+</html>
